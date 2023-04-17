92a2e3cae2f84a3375f150d3577fa2d185dc98cf
Ultron03 <100458211+Ultron03@users.noreply.github.com>
2023-04-17 17:08:28 +0530

    Space Arcade 2D Game

---
+++ menu.py
@@ -0,0 +1,128 @@
+import pygame
+import sys
+import random
+import pygame.mixer
+
+
+WIDTH, HEIGHT = 1200, 800
+FPS = 60
+WHITE = (154, 164, 166)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+
+
+def animate_screen():
+    for i in range(0, 20):
+        screen.blit(mainmenu_img, (0, 0))
+        pygame.display.flip()
+        pygame.time.wait(10)
+        screen.blit(mainmenu_img, (random.randint(-5, 5), random.randint(-5, 5)))
+        pygame.display.flip()
+        pygame.time.wait(10)
+
+
+pygame.mixer.init()
+pygame.init()
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Main Menu")
+clock = pygame.time.Clock()
+
+mainmenu_img = pygame.image.load('images/I.png').convert()
+mainmenu_img = pygame.transform.scale(mainmenu_img, (WIDTH, HEIGHT))
+
+
+play_button_rect = pygame.Rect(WIDTH // 2 - 100, HEIGHT // 2 - 25, 205, 50)
+quit_button_rect = pygame.Rect(WIDTH // 2 - 100, HEIGHT // 2 + 60, 205, 80)
+
+pygame.mixer.music.load('game_sounds/menu.mp3')
+pygame.mixer.music.play(-1)
+explosion_sound = pygame.mixer.Sound('game_sounds/explosions/explosion1.wav')
+
+selected_button = 0
+show_menu = True
+
+joystick = None
+if pygame.joystick.get_count() > 0:
+    joystick = pygame.joystick.Joystick(0)
+    joystick.init()
+
+
+while show_menu:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            x, y = event.pos
+            if play_button_rect.collidepoint(x, y):
+                explosion_sound.play()
+                animate_screen()
+                show_menu = False
+                import game
+                game.main()
+                break
+            elif quit_button_rect.collidepoint(x, y):
+                pygame.quit()
+                sys.exit()
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                selected_button = 0
+            elif event.key == pygame.K_DOWN:
+                selected_button = 1
+            elif event.key == pygame.K_RETURN:
+                if selected_button == 0:
+                    explosion_sound.play()
+                    animate_screen()
+                    show_menu = False
+                    screen.fill(BLACK)
+                    import game
+                    game.main()
+                    break
+                elif selected_button == 1:
+                    pygame.quit()
+                    sys.exit()
+
+        if joystick:
+            if event.type == pygame.JOYBUTTONDOWN:
+                if event.button == 0:
+                    if selected_button == 0:
+                        explosion_sound.play()
+                        animate_screen()
+                        show_menu = False
+                        screen.fill(BLACK)
+                        import game
+                        game.main()
+                        break
+                    elif selected_button == 1:
+                        pygame.quit()
+                        sys.exit()
+            elif event.type == pygame.JOYHATMOTION:
+                if event.value[1] == 1:
+                    selected_button = 0
+                elif event.value[1] == -1:
+                    selected_button = 1
+
+    screen.blit(mainmenu_img, (0, 0))
+
+
+    font = pygame.font.SysFont('Comic Sans MS', 40)
+    text = font.render("", True, WHITE)
+
+    text_rect = text.get_rect()
+    text_rect.center = play_button_rect.center
+    screen.blit(text, text_rect)
+
+    text = font.render("", True, WHITE)
+    if selected_button == 1:
+        pygame.draw.rect(screen, RED, quit_button_rect, border_radius=12, width=6)
+    text_rect = text.get_rect()
+    text_rect.center = quit_button_rect.center
+    screen.blit(text, text_rect)
+
+    pygame.display.flip()
+    clock.tick(60)
+
+pygame.quit()
